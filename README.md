3a21df95fe7f1b9f5b5bb607cba87b0bfa1f1c35
Barrett <barrett.amos@gmail.com>
2012-09-14 01:13:28 -0700

    Splitting from minion-daemon

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-minion-log
-==========
+# minion-log
+
+`minion-log` is an extension for [Kohana Minion](https://github.com/kohana-minion/core) to more easily handle logging from the command line.
+
+## Installation
+
+`minion-log` should be added to your Kohana `MODPATH` directory.  Then enable it in the `modules` section of your bootstrap.
+
+## Requirements
+
+* [kohana-minion](https://github.com/kohana-minion/core) is not strictly required, but this was written with minion/command line interfaces in mind.
+
+## Compatibility
+* Written for Kohana 3.2.
+
+## Usage
+
+This module extends the Kohana `Log` class and can be used in the same manner.  Remember that you'll need to attach `Log_Writer`s.
+
+### Write On Add
+
+The main addition in this module is the ability to write to a given writer as soon as the `$log->add()` method is called.  This is useful in CLI situations where you may be writing to multiple writers.  For example, you may want to have output written immediately to StdOut, but delay writing to a Log File to avoid hammering system I/O.
+
+This works by passing an optional fourth additional parameter to `$log->attach()`.  If the parameter is `TRUE`, output to that log writer will be written immediatly as soon as $log->add() is called.  If the parameter is `FALSE` (default), output won't be written until $log->write() is called.
+
+### Example  
+This is an example of using Minion_Log:
+
+	$log = Minion_Log::instance(); // Or, just new Minion_Log
+	
+	// Add the log file writer
+	$log->attach(new Log_File(APPPATH.'logs'));
+	
+	// Add StdOut.  Pass the other default parameters and the new $write_on_add parameter
+	$log->attach(new Log_StdOut, array(), 0, TRUE);
+	
+	// This will immediately write to StdOut.
+	// Output will be buffered for the log file until $log->write() is called or the shutdown function is called
+	$log->add('Foobar');
+	
+	// This will write to the log file, but not to StdOut (since that output was pushed immediately)
+	$log->write();
+	
+## Testing
+
+This module is unittested using the [unittest module](http://github.com/kohana/unittest).
+You can use the `minion.log` group to only run minion log tests.  It is also grouped under `minion`.
+
+i.e.
+
+	phpunit --group minion.log
+
+## Bugs?  Issues?
+
+That's why this is hosted on github :).  Feel free to fork it, submit issues, or patches.
+
+## License
+
+This is licensed under the [same license as Kohana](http://kohanaframework.org/license).
---
+++ tests/minion/log.php
@@ -0,0 +1,153 @@
+<?php
+
+/**
+ * Test case for Minion_Log
+ *
+ * @group minion
+ * @group minion.log
+
+ */
+class Minion_LogTest extends Kohana_Unittest_TestCase
+{
+	/**
+	 * Can we instantiate the log?
+	 * 
+	 * @access public
+	 * @return void
+	 */
+	function test_log()
+	{
+		$log = new Minion_Log;
+		
+		$this->assertInstanceOf('Kohana_Log',$log);
+	}
+	
+	/**
+	 * Make sure we aren't overriding Kohana::log and that 
+	 * Make sure Minion_Log::instance() is an instance of Minion_Log (not just Log)
+	 *
+	 * @access public
+	 * @return void
+	 */
+	function test_instance()
+	{
+		$log = Minion_Log::instance();
+		
+		$this->assertInstanceOf('Minion_Log', $log);
+		
+		// assertNotInstanceOf doesn't work here!?
+		$this->assertFalse(is_a(Kohana::$log, 'Minion_Log'));
+	}
+	
+	/**
+	 * Attach a regular writer and a write-on-add writer
+	 * 
+	 * @access public
+	 * @return void
+	 */
+	function test_attach_writer()
+	{
+		$log = new Minion_Log;
+		
+		$writer = $this->getMockForAbstractClass('Log_Writer');
+		$writer_expects = array(
+			spl_object_hash($writer) => array(
+				'object' => $writer,
+				'levels' => array(Log::NOTICE)));
+		
+		$on_add = $this->getMockForAbstractClass('Log_Writer');
+		$on_add_expects = array(
+			spl_object_hash($on_add) => array(
+				'object' => $on_add,
+				'levels' => array(Log::NOTICE)));
+		
+		$this->assertSame($log, $log->attach($writer, array(Log::NOTICE)), "Minion_Log should not change after attaching a writer");
+		$this->assertAttributeSame($writer_expects, '_writers', $log, "The writer should be added to the _writers array");
+		
+		$this->assertSame($log, $log->attach($on_add, array(Log::NOTICE), 0, TRUE), "Minion_Log should not change after attaching a write-on-add writer");
+		$this->assertAttributeSame($writer_expects, '_writers', $log, "_writers should not be affected when adding a _write_on_add writer");
+		$this->assertAttributeSame($on_add_expects, '_on_add_writers', $log, "_write_on_add_writers should be updated after attaching a new writer-on-add writer");
+		
+	}
+	
+	/**
+	 * Detach a regular writer and a write-on-add writer
+	 * 
+	 * @access public
+	 * @return void
+	 */
+	function test_detach_writer()
+	{
+		$log = new Minion_Log;
+		$writer = $this->getMockForAbstractClass('Log_Writer');
+		$on_add = $this->getMockForAbstractClass('Log_Writer');
+		
+		$log->attach($writer);
+		$log->attach($on_add,array(),0,TRUE);
+		
+		$this->assertAttributeNotEmpty('_writers', $log, "_writer should be attached");
+		$this->assertAttributeNotEmpty('_on_add_writers', $log, "_on_add_writer should be attached");
+		
+		$log->detach($writer);
+		$this->assertAttributeEmpty('_writers', $log, "_writer should not be attached");
+		$this->assertAttributeNotEmpty('_on_add_writers', $log, "_on_add_writer should be attached");
+		
+		$log->detach($on_add);
+		$this->assertAttributeEmpty('_writers', $log, "_writer should not be attached");
+		$this->assertAttributeEmpty('_on_add_writers', $log, "_on_add_writers should not be attached");
+	}
+	
+	/**
+	 * Test $log->add()
+	 * 
+	 * @access public
+	 * @return void
+	 */
+	function test_add()
+	{
+		$log = new Minion_Log;
+		$writer = $this->getMockForAbstractClass('Log_Writer');
+		$on_add = $this->getMockForAbstractClass('Log_Writer');
+		
+		$log->attach($writer);
+		$log->attach($on_add,array(),0,TRUE);
+		
+		// Should only be called on $log->write()
+		$writer->expects($this->never())
+			->method('write');
+			
+		// Should only be called on $log->add()
+		$on_add->expects($this->once())
+			->method('write');
+			
+		$log->add(Log::INFO, "foobar");
+	}
+	
+	/**
+	 * Test $log->write()
+	 * 
+	 * @access public
+	 * @return void
+	 */
+	function test_write()
+	{
+		$log = new Minion_Log;
+		$writer = $this->getMockForAbstractClass('Log_Writer');
+		$on_add = $this->getMockForAbstractClass('Log_Writer');
+		
+		$log->attach($writer);
+		$log->attach($on_add,array(),0,TRUE);
+		
+		// Should be called on $log->write()
+		$writer->expects($this->once())
+			->method('write');
+		
+		// Should be called on $log->add()
+		$on_add->expects($this->once())
+			->method('write');
+			
+		$log->add(Log::INFO, "foobar");
+		
+		$log->write();
+	}
+}
